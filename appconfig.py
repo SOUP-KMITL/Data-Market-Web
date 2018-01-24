dde9317df5c55dc67c6c3d312acc03d2b7036949
Kohpai <whcwhc78@gmail.com>
2018-01-24 15:33:38 +0700

    fix login procedure

--- appconfig.py
+++ appconfig.py
@@ -9,7 +9,7 @@ class DefaultConfig(object):
 
 
 API_VER = 1
-EXT_API_PORT = 80
+EXT_API_PORT = 443
 INT_API_PORT = 8080
 SECRET = "some secret"
 
@@ -23,8 +23,8 @@ if env == "PROD":
     METER_API      = "http://meter-service:" + str(INT_API_PORT) + "/api/v1/meters"
 else:
     #  Development (local) configuration
-    #  EXT_API_GATEWAY = "http://api.smartcity.kmitl.io:" + str(EXT_API_PORT) + "/api/v1"
-    EXT_API_GATEWAY = "http://203.154.59.55:" + str(EXT_API_PORT) + "/api/v1"
+    EXT_API_GATEWAY = "https://api.smartcity.kmitl.io:" + str(EXT_API_PORT) + "/api/v1"
+    #  EXT_API_GATEWAY = "https://203.154.59.55:" + str(EXT_API_PORT) + "/api/v1"
     API_PREFIX     = "/api/v" + str(API_VER) + "/web"
     COLLECTION_API = EXT_API_GATEWAY + "/collections"
     USER_API       = EXT_API_GATEWAY + "/users"
--- index.py
+++ index.py
@@ -11,72 +11,78 @@ app = Flask(__name__)
 app.config.from_object("appconfig.DefaultConfig")
 CORS(app)
 
+# @TODO
+# Remove this when all data are stored in data exchange layer
 temp_data = json.load(open('service.json'))
 
 
 @app.route(appconfig.API_PREFIX + "/login/", methods=['POST'])
 def login():
-    username = request.form.get("username", "")
-    password = request.form.get("password", "")
-    retResp = {"status": "failed", "message": "", "data": None}
+    retResp = {"success": False, "message": ""}
     httpCode = 400
+
+    if not request.is_json:
+        retResp["message"] = "Invalid request body type, expected JSON"
+        return jsonify(retResp), httpCode
+
+    incomData = request.get_json()
+    username = incomData.get("username", "")
+    password = incomData.get("password", "")
 
     try:
         resp = requests.get(appconfig.LOGIN_API, auth=(username, password))
-    except requests.ConnectionError:
-        retResp["message"] = "couldn't connect to external service"
-    except requests.ConnectTimeout:
-        retResp["message"] = "connection to external service timeout"
+    except (requests.ConnectionError, requests.ConnectTimeout) as e:
+        retResp["message"] = e.__str__()
+        httpCode = 500
     else:
-        retResp["message"] = getRtrnMsg(resp.status_code)
+        httpCode = resp.status_code
 
         if resp.status_code == 200:
-            retResp["status"] = "successful"
-            retResp["data"] = {
-                    "access_token": jwt.encode({
-                        "rol": resp.json().get("role", "user"),
-                        "key": resp.json().get("accessToken", "")},
-                        appconfig.SECRET,
-                        algorithm="HS256").decode(),
-                    "userId": resp.json().get("userId", "")}
-            httpCode = 200
+            respData = resp.json()
+            accessToken = respData.get("accessToken", "")
+            webAccessToken = jwt.encode(
+                    {"key": accessToken},
+                    appconfig.SECRET,
+                    algorithm='HS256').decode()
+            retResp = {
+                    "access_token": webAccessToken,
+                    "userId": respData.get("userId", "")}
+        else:
+            retResp["message"] = getRtrnMsg(resp.status_code)
     finally:
         return jsonify(retResp), httpCode
 
 
-@app.route(appconfig.API_PREFIX + "/dataBuckets/")
+@app.route(appconfig.API_PREFIX + "/dataCollections/")
 def getDataBuckets():
     query = {"open": True}
-    retResp = {"status": "failed", "message": "", "data": []}
+    retResp = {"success": False, "message": ""}
     httpCode = 400
 
     try:
         resp = requests.get(appconfig.COLLECTION_API, params=query)
-    except requests.ConnectionError:
-        retResp["message"] = "couldn't connect to external service"
-    except requests.ConnectTimeout:
-        retResp["message"] = "connection to external service timeout"
+    except (requests.ConnectionError, requests.ConnectTimeout) as e:
+        retResp["message"] = e.__str__()
+        httpCode = 500
     else:
-        retResp["message"] = getRtrnMsg(resp.status_code)
+        httpCode = resp.status_code
 
         if resp.status_code == 200:
-            retResp["status"] = "successful"
-            retResp["data"] = resp.json()
-            httpCode = 200
+            retResp = resp.json()
+        else:
+            retResp["message"] = getRtrnMsg(resp.status_code)
     finally:
         return jsonify(retResp), httpCode
 
 
 @app.route(appconfig.API_PREFIX + "/cityServices/")
 def getCityServices():
-    return jsonify({
-        "status": "successful",
-        "message": "This is just a temporary data.",
-        "data": temp_data}), 200
+    return jsonify(temp_data), 200
 
 
 def getRtrnMsg(code):
     return {
             400: "Bad request",
+            401: "Wrong username or password",
             500: "external service error",
             200: ""}.get(code, "Unknown external service error: " + str(code))
