b602e3570b51bee2dd3e1e9b5dfe4b40cb112a0b
Kohpai <whcwhc78@gmail.com>
2018-01-25 18:51:45 +0700

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 if [ -z "$1" ] || [ -z "$2" ]
 then
     echo "rsync -azrP --exclude-from \"rsync.exc\" --include \".dockerignore\" \
-        ./ smartCity-dataMarket-tmp:/home/centos/kohpai/WebService/"
+        ./ api-smartCity:/home/centos/kohpai/WebService/"
     rsync -azrP --exclude-from "rsync.exc" --include ".dockerignore" \
-        ./ smartCity-dataMarket-tmp:/home/centos/kohpai/WebService/
+        ./ api-smartCity:/home/centos/kohpai/WebService/
 else
     echo "rsync -azrP --exclude-from \"rsync.exc\" --include \".dockerignore\" ./ $1:$2"
     rsync -azrP --exclude-from "rsync.exc" --include ".dockerignore" ./ $1:$2
